6dc8f9f4fb0ddeac2f9a0d9d105dafdd812d73cd
Manav Modi <102470112+manavnmodi111@users.noreply.github.com>
2026-01-22 10:09:50 +0000

    app.js changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const VERSION = process.env.IMAGE_TAG || 'local';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello Prod Argocd: ${VERSION}\n`);
+  res.end(`Hello without webhook: ${VERSION}\n`);
 });
 
 server.listen(PORT, () => {
