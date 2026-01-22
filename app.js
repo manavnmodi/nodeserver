f4e4d8aab68840b6c136e93bdd29cba5f93dcb11
Manav Modi <102470112+manavnmodi111@users.noreply.github.com>
2026-01-22 11:00:53 +0000

    webhook updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const VERSION = process.env.IMAGE_TAG || 'local';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello with webhook4: ${VERSION}\n`);
+  res.end(`Hello with webhook5: ${VERSION}\n`);
 });
 
 server.listen(PORT, () => {
