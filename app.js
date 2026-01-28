5394302e3bdb041c58ae661e2b235f3468417338
Manav Modi <102470112+manavnmodi111@users.noreply.github.com>
2026-01-28 09:26:32 +0000

    app.js updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const VERSION = process.env.IMAGE_TAG || 'local';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello DevOps Engineer: ${VERSION}\n`);
+  res.end(`Hello DevOps Engineer PRO: ${VERSION}\n`);
 });
 
 server.listen(PORT, () => {
