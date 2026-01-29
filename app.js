6300514f409ef4ca5a48d204a1148cca37883889
Manav Modi <102470112+manavnmodi111@users.noreply.github.com>
2026-01-29 08:46:38 +0000

    app.js updated

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const VERSION = process.env.IMAGE_TAG || 'local';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello Inventyv DevOps
-    : ${VERSION}\n`);
+  res.end(`Hello Inventyv DevOps: ${VERSION}\n`);
 });
 
 server.listen(PORT, () => {
