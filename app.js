2eb8548b86978763bb38d9eac57bd84b95b1b62a
Manav Modi <102470112+manavnmodi111@users.noreply.github.com>
2026-03-26 15:27:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello Inventyv DevOps: ${VERSION}\n`);
 });
-
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
